28a02057df71e0df5e0d8afc1885336018f4239c
Mitchel Noble <Mitch@Mitchels-MBP.lan>
2021-01-29 14:33:24 -0800

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,89 +3,89 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ title: 'Star Wars' }, { title: 'Lord of the Rings' }])
+#   Character.create(title: 'Luke', movie: movies.first)
 
 vinyl_list = Vinyl.create([
   {
-    name:"Choose Your Weapon",
+    title:"Choose Your Weapon",
     artist:"Hiatus Kaiyote",
     price:30.99,
     description:"Double vinyl LP pressing. Choose Your Weapon is the second studio album by Australian neo-soul quartet Hiatus Kaiyote, first released in 1 May 2015. Following the release of the band's debut album Tawk Tomahawk (2012) and their first Grammy nomination, Hiatus Kaiyote went on to tour throughout 2014. Over the span of two years, recording sessions took place in Australia and the United States.",
     img_url:"../assets/Vinyl Images/Vinyl 1/3808402-2553669_400x.jpg",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
     img_url:"",
   },
   {
-    name:"",
+    title:"",
     artist:"",
     price:"",
     description:"",
