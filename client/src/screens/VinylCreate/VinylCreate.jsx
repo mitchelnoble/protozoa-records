1eb6eda05d52901195075ab2f30d2d77238300a8
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2021-02-01 13:08:20 -0800

    added vinyl detail and create

--- client/src/screens/VinylCreate/VinylCreate.jsx
+++ client/src/screens/VinylCreate/VinylCreate.jsx
@@ -1,51 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function VinylCreate(props) {
   const [formData, setFormData] = useState({
-    name: ''
-  })
+    name: "",
+  });
   const { name } = formData;
-  const {handleCreate} = props;
+  const { handleCreate } = props;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      handleCreate(formData)
-    }}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleCreate(formData);
+      }}
+    >
       <h3>Sell Your Record!</h3>
-      <label>Title:
+      <label>
+        Title:
+        <input type="text" name="Title" value={name} onChange={handleChange} />
+      </label>
+      <label>
+        Artist:
+        <input type="text" name="Artist" value={name} onChange={handleChange} />
+      </label>
+      <label>
+        Price:
         <input
-          type='text'
-          name='Title'
+          type="number"
+          name="Price"
           value={name}
           onChange={handleChange}
         />
       </label>
-      <label>Artist:
-        <input
-          type='text'
-          name='Artist'
+      <label>
+        Description:
+        <textarea
+          name="Description"
           value={name}
+          rows="10"
+          cols="30"
           onChange={handleChange}
         />
       </label>
-      <label>Artist:
+      <label>
+        Genre:
+        <input type="text" name="Genre" value={name} onChange={handleChange} />
+      </label>
+      <label>
+        Image Link:
         <input
-          type='text'
-          name='Artist'
+          type="url"
+          name="Image Link"
           value={name}
           onChange={handleChange}
         />
       </label>
       <button>Submit</button>
     </form>
-  )
+  );
 }
--- client/src/screens/VinylDetail/VinylDetail.jsx
+++ client/src/screens/VinylDetail/VinylDetail.jsx
@@ -1,26 +1,22 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getOneVinyl } from '../services/vinyls';
+import React from 'react';
+
+
 
 export default function VinylDetail(props) {
-  const [vinylItem, setVinylItem] = useState(null);
-  
-  const { id } = useParams();
-
-  useEffect(() => {
-    const fetchVinylItem = async () => {
-      const vinylData = await getOneVinyl(id);
-      setVinylItem(vinylData);
-    }
-    fetchVinylItem();
-  }, [id])
+  const { vinyl } = props;
 
   return (
-    <div>
-      <h3>{vinylItem?.name}</h3>
-      {foodItem?.flavors.map(flavor => (
-        <p key={flavor.id}>{flavor.name}</p>
-      ))}
-    </div>
-  )
-}
+    <Link className="vinyl" to={`/detail/${vinyl._id}`}>
+      <img
+        className="vinyl-image"
+        src={vinyl.img_url[0].image}
+        alt={props.title}
+      />
+      <div className="vinyl-title">{vinyl.title}</div>
+      <div className="vinyl-artist">{`${vinyl.artist}`}</div>
+      <div className="vinyl-price">{`${vinyl.price}`}</div>
+      <div className="vinyl-description">{`${vinyl.description}`}</div>
+      <div className="vinyl-genre">{`${vinyl.genre}`}</div>
+    </Link>
+  );
+};
