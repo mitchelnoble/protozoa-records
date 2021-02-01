68451378f3ad793c36bd4db68960b8030fc33f1b
Mitchel Noble <Mitch@Mitchels-MBP.lan>
2021-02-01 08:12:25 -0800

    working on CRUD front end

--- client/src/containers/VinylContainer/VinylContainer.jsx
+++ client/src/containers/VinylContainer/VinylContainer.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import VinylCreate from "../../screens/VinylCreate/VinylCreate";
+import VinylEdit from "../../screens/VinylEdit/VinylEdit";
+import VinylDetail from "../../screens/VinylDetail/VinylDetail";
+import {
+  deleteVinyl,
+  getAllVinyls,
+  postVinyl,
+  putVinyl,
+} from "../../services/vinyls";
 
-export default function VinylContainer() {
+export default function VinylContainer(props) {
+  const [vinyls, setVinyls] = useState([]);
+  const history = useHistory();
+  const { currentUser } = props;
+
+  useEffect(() => {
+    const fetchVinyls = async () => {
+      const vinylData = await getAllVinyls();
+      setVinyls(vinylData);
+    };
+    fetchVinyls();
+  }, []);
+
+  const handleCreate = async (vinylData) => {
+    const newVinyl = await postVinyl(vinylData);
+    setVinyls((prevState) => [...prevState, newVinyl]);
+    history.push("/vinyls");
+  };
+
+  const handleDelete = async (id) => {
+    await deleteVinyl(id);
+    setVinyls((prevState) =>
+      prevState.filter((vinylItem) => {
+        return vinylItem.id !== id;
+      })
+    );
+  };
+
+  const handleUpdate = async (id, vinylData) => {
+    const updatedVinyl = await putVinyl(id, vinylData);
+    setVinyls((prevState) =>
+      prevState.map((vinylItem) => {
+        return vinylItem.id === Number(id) ? updatedVinyl : vinylItem;
+      })
+    );
+    history.push("/vinyls");
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Switch>
+      <Route path="/vinyls/:id/edit">
+        <VinylEdit vinyls={vinyls} handleUpdate={handleUpdate} />
+      </Route>
+      <Route path="/vinyls/:id">
+        <VinylDetail vinyls={vinyls} />
+      </Route>
+      <Route path="/vinyls/new">
+        <VinylCreate handleCreate={handleCreate} />
+      </Route>
+      <Route path="/vinyls">
+        <VinylList
+          vinyls={vinyls}
+          handleDelete={handleDelete}
+          currentUser={currentUser}
+        />
+      </Route>
+    </Switch>
+  );
 }
