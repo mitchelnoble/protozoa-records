36ba254513e34d38a3fb2ed6b6be2a6d2f1c9745
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-02 14:54:16 -0800

    updated seed data photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ vinyl_list = Vinyl.create!([
     artist:"Hiatus Kaiyote",
     price:30.99,
     description:"Double vinyl LP pressing. Choose Your Weapon is the second studio album by Australian neo-soul quartet Hiatus Kaiyote, first released in 1 May 2015. Following the release of the band's debut album Tawk Tomahawk (2012) and their first Grammy nomination, Hiatus Kaiyote went on to tour throughout 2014. Over the span of two years, recording sessions took place in Australia and the United States.",
-    img_url:"https://i.imgur.com/lAGd8vSs.jpg",
+    img_url:"https://i.imgur.com/lAGd8vSb.jpg",
     genre: ["Funk ", "Soul ", "R&B "]
   },
   {
@@ -24,7 +24,7 @@ vinyl_list = Vinyl.create!([
     artist:"Moonchild",
     price:25.99,
     description:"The third full-length album from LA's Moonchild, whose candid style of soul and new-school jazz has propelled them onto a swirling, emotionally charged journey of the heart.",
-    img_url:"https://i.imgur.com/K4iO6kFs.jpg",
+    img_url:"https://i.imgur.com/K4iO6kFb.jpg",
     genre: ["Soul ", "R&B "]
   },
   {
@@ -40,7 +40,7 @@ vinyl_list = Vinyl.create!([
     artist:"SG Lewis",
     price:49.99,
     description:"A 3-part limited edition LP box set, trifolding in spot-varnished sleeves.",
-    img_url:"https://i.imgur.com/8adnFH1.jpg",
+    img_url:"https://i.imgur.com/8adnFH1b.jpg",
     genre: ["Electronic ", "Dance "]
   },
   {
@@ -48,7 +48,7 @@ vinyl_list = Vinyl.create!([
     artist:"Tom Misch",
     price:24.99,
     description:"Beat Tape 2 follows a string of well-received releases since it's predecessor Beat Tape 1, including his Out To Sea E.P with Carmody, a white label release on hip-hop label Soulection, collaborations with the likes of Bearcubs and reworks of Mos Def and Busta Rhymes. On Beat Tape 2, Tom has evolved his sound from it's origins in Boom Bap hip-hop stylings of producers like J Dilla to encompass his diverse range of influences. Tom's music finds the sweet spot between electronica, hip-hop and acoustic, predominantly linked by a single quality: soul. 'Its about the feeling you get from the music that makes it soulful, and for me that comes down to the chords. If it's a nice, soulful chord progression that's what makes it soulful and gives you that warm feeling. A lot of Gospel music, J Dilla, Erykah Badu and Jazz - I find that very soulful. That's what I listen to, so I guess that comes out in my music.'",
-    img_url:"https://i.imgur.com/hXRr9Nu.jpg",
+    img_url:"https://i.imgur.com/hXRr9Nub.jpg",
     genre: ["Soul ", "Jazz ", "R&B "]
   },
   {
@@ -56,7 +56,7 @@ vinyl_list = Vinyl.create!([
     artist:"Alina Baraz",
     price:19.99,
     description:"Urban Flora, the 2015 debut EP by Alina Baraz and Galimatias, is available in physical form for the first time ever! The album is pressed on 150 gram clear vinyl with custom packaging that includes vinyl inner sleeves with the album artwork and a double-sided insert featuring single artwork from “Fantasy” and “Maybe.”",
-    img_url:"https://i.imgur.com/xGOEiH3.png",
+    img_url:"https://i.imgur.com/xGOEiH3b.png",
     genre: ["Electronic ", "Soul "]
   },
   {
@@ -64,7 +64,7 @@ vinyl_list = Vinyl.create!([
     artist:"Disclosure",
     price:35.99,
     description:"2013 album from the British Electronic duo consisting of brothers Gay and Howard Lawrence, Disclosure have been making waves across the pond with their revival of the UK Garage/House sound marked by advanced rhythm combinations and soulful R&B vocal samples. The album includes appearances from Eliza Doolittle, Jamie Woon, Jessie Ware, Ed Mac, Sasha Keable, and others.",
-    img_url:"https://i.imgur.com/fMzOH8V.jpg",
+    img_url:"https://i.imgur.com/fMzOH8Vb.jpg",
     genre: ["House ", "Dance "]
   },
   {
@@ -72,7 +72,7 @@ vinyl_list = Vinyl.create!([
     artist:"French Kiwi Juice",
     price:32.99,
     description:"Double vinyl LP pressing. Multi-instrumentalist with rare elegance, FKJ unveils it's first album French Kiwi Juice, and invites us in his world. Tribute to his idols, affirmation of musical maturity, FKJ goes from Jazz to Soul, with his electronic touch that makes his paw incomparable. A warm, reassuring and sensual universe. His children's questions (Why are there boundaries), his joys (Blessed & Joy), his journeys (Go Back Home & Canggu), the Franco-New Zealander delivers himself in an intimate and profound album, with music as an universal language.",
-    img_url:"https://i.imgur.com/dnAPi4M.jpg",
+    img_url:"https://i.imgur.com/dnAPi4Mb.jpg",
     genre: ["Jazz ", "Electronic ", "Soul ", "Funk "]
   },
   {
@@ -80,7 +80,7 @@ vinyl_list = Vinyl.create!([
     artist:"Daft Punk",
     price:239.99,
     description:"Discovery is the second studio album by French electronic music duo Daft Punk, released on 26 February 2001 by Virgin Records. It marks a shift from the Chicago house sound prevalent on their first studio record, Homework (1997), to a house style more heavily inspired by disco, post-disco, garage house, and R&B. Comparing their stylistic approach to their previous album, band member Thomas Bangalter described Discovery as an exploration of song structures and musical forms whereas Homework was 'raw' electronic music. He also described Discovery as a reflection of the duo's childhood memories, when they listened to music with a more playful and innocent viewpoint.",
-    img_url:"https://i.imgur.com/3sI59xB.jpg",
+    img_url:"https://i.imgur.com/3sI59xBb.jpg",
     genre: ["House ", "Dance ", "Electronic "]
   },
   {
@@ -88,7 +88,7 @@ vinyl_list = Vinyl.create!([
     artist:"Bill Evans",
     price:21.99,
     description:"After Scott LaFaro's tragic death, Evans avoided the studio for some time. He returned in May and June of 1962 to record this album of ballads with Chuck Israels and Paul Motion.",
-    img_url:"https://i.imgur.com/n5vWpHR.jpg",
+    img_url:"https://i.imgur.com/n5vWpHRb.jpg",
     genre: ["Jazz"]
   },
   {
@@ -96,7 +96,7 @@ vinyl_list = Vinyl.create!([
     artist:"D'Angelo",
     price:33.99,
     description:"D'Angelo's debut solo album, Brown Sugar, released in July 1995, received rave reviews and was certified Platinum in the US. Following this, D'Angelo embarked on a hiatus before releasing Voodoo in January 2000 which debuted at number one, its lead single 'Untitled (How Does It Feel)', was a smash on the R&B charts and won a Grammy for Best Male R&B Vocal; likewise, Voodoo won for Best R&B Album.",
-    img_url:"https://i.imgur.com/lEb0NJY.png",
+    img_url:"https://i.imgur.com/lEb0NJYb.png",
     genre: ["Soul ", "R&B "]
   },
   {
@@ -104,7 +104,7 @@ vinyl_list = Vinyl.create!([
     artist:"Gallant",
     price:29.99,
     description:"The push-and-pull that drives Gallant's music is like a pendulum swinging back and forth between genres and influences. His combination of muscular vocal acrobatics and sonic unpredictability has attracted universal tastemaker praise and a growing fan base. Complex championed the Los Angeles-based singer and songwriter as 'The R&B game changer,' LA Times claimed, 'Precise writing of Frank Ocean, big vocal runs of Sam Smith,' and NME predicted, 'the voice that will redefine R&B,' while Entertainment Weekly, The Fader, Billboard, and more have shown support. Sir Elton John summed it up best saying, 'Gallant is gonna be huge.'",
-    img_url:"https://i.imgur.com/3Ftypmx.jpg",
+    img_url:"https://i.imgur.com/3Ftypmxb.jpg",
     genre: ["R&B ","Soul "]
   },
 ])
