cdea8e9fe0eaf93aebf08b7309607d838fa8bdcf
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-02 15:10:10 -0800

    added current user to edit link on vinyl detail

--- client/src/screens/VinylDetail/VinylDetail.jsx
+++ client/src/screens/VinylDetail/VinylDetail.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 
 export default function VinylDetail(props) {
-  // const { vinyls } = props;
-  console.log(props);
+  const { vinyls, currentUser } = props;
+  console.log(vinyls);
 
   return (
     <>
       <img
-        className="vinyls-image"
-        src={props.img_url}
-        alt={props.title}
-      />
-      <div className="vinyls-title">{props.title}</div>
-      <div className="vinyls-artist">{`${props.artist}`}</div>
-      <div className="vinyls-price">{`${props.price}`}</div>
-      <div className="vinyls-description">{`${props.description}`}</div>
-      <div className="vinyls-genre">{`${props.genre}`}</div>
+        className="vinyl-image"
+        src={vinyls.img_url}
+        alt={vinyls.title}
+        />
+      <div className="vinyls-title">{vinyls.title}</div>
+      <div className="vinyls-artist">{`${vinyls.artist}`}</div>
+      <div className="vinyls-price">{`${vinyls.price}`}</div>
+      <div className="vinyls-description">{`${vinyls.description}`}</div>
+      <div className="vinyls-genre">{`${vinyls.genre}`}</div>
+      {currentUser ? <Link to="/vinyls/:id/edit">Edit/Delete</Link> : null}
       </>
   );
 };
--- client/src/screens/VinylList/VinylList.jsx
+++ client/src/screens/VinylList/VinylList.jsx
@@ -8,23 +8,23 @@ export default function VinylList(props) {
   // const history = useHistory();
   console.log(vinyls);
   return (
-    <div className="vinyl-container">
+    <div className="vinyls-container">
       {vinyls.map((vinyl) => {
         return (
-          <>
             <Link to={`/vinyls/${vinyls.id}:id`}>
+          <div className="vinyl-container">
             <img
         className="vinyl-image"
         src={vinyl.img_url}
         alt={vinyl.name}
         />
-           <h3>{vinyl.artist}</h3>
             <h1>{vinyl.title}</h1>
+           <h3>{vinyl.artist}</h3>
             <h3>${vinyl.price}</h3>
             <h3>{vinyl.genre}</h3>
-        </Link>
             <br />
-            </>
+            </div>
+        </Link>
         )
       })}
         </div>
